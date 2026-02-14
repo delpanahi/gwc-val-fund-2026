cbd476a87cd80f6b7295981e0d7e747befc1bceb
delpanahi <panahidr@clarkson.edu>
2026-02-13 23:16:45 -0500

    new

---
+++ Penniel/static/type.js
@@ -0,0 +1,26 @@
+// const text = 'Dum haec verba scribere incipio, calore repleor qui sola tua praesentia in vita mea affertur. Omni momento transeunte, amor meus erga te profundior fit, altior et omnino sine conditionibus. Tecum, amorem inveni qui ordinem transcendit, nexum qui rationem superat. Tu mihi consiliaria et maxima laetitiae fons es. In amplexu tuo domum repperi, et in amore tuo propositum vitae meae inveni. In perpetuum et semper, tuus sum.';
+// const text3 = "With all my love";
+
+
+// let i = 0, j = 0;
+// const speed = 50;
+
+// function typeWriter1() {
+//   if (i < text.length) {
+//     document.getElementById("tpewriter").textContent += text.charAt(i);
+//     i++;
+//     setTimeout(typeWriter1, speed);
+//   } else {
+//     setTimeout(typeWriter2, 1000); // Delay before typing the second text
+//   }
+// }
+
+// function typeWriter2() {
+//   if (j < text3.length) {
+//     document.getElementById("signature").textContent += text3.charAt(j);
+//     j++;
+//     setTimeout(typeWriter2, speed);
+//   }
+// }
+
+// window.onload = typeWriter1;
